d28da60d4d88dd141e35460ed6f39e4e6357f3e7
imad-el-hajjami <contact@imadelhajjami.com>
2023-09-13 15:01:42 -0700

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_ite
+    save_to_json_file(items, "add_item.json")
 
